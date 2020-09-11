5c7ff17ceedde167a4172c932c46d2916fee17d5
larisa <larisa@merchise.org>
2020-09-11 00:01:16 -0400

    Add preconditions validations for stage workflow on recruitment pipeline.
    This must be an standard procedure, so every time a "precondition" emerge,
    there is only needed to register a new _stage_preconditions method.

---
+++ addons/hr_recruitment_stage_preconditions/models/hr_recruitment.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, models
+from odoo.tools.translate import _
+from odoo.exceptions import ValidationError
+
+
+class Applicant(models.Model):
+    _inherit = ['hr.applicant']
+
+    @api.onchange('stage_id')
+    def onchange_stage_id(self):
+        if self._check_preconditions():
+            return super(Applicant, self).onchange_stage_id()
+
+    def _check_preconditions(self):
+        stage = self.env['hr.recruitment.stage'].browse(self.stage_id.id)
+        method_name = "_" + str.lower(stage.name.replace(" ", "_")) + "_preconditions"
+        try:
+            method = getattr(self, method_name)
+            fulfilled = method()
+        except ValidationError as e:
+            # Catch all the validations errors thrown on _precondition methods
+            raise ValidationError(e.name)
+        except:
+            # If the _preconditions method doesn't exist
+            # stage change should be done
+            fulfilled = True
+        return fulfilled
